39b708a49e0aaa04b4b57af1e78904bcd6ed890f
Sumit <99789065+s198p@users.noreply.github.com>
2022-07-22 07:20:57 +0530

    Add files via upload

--- tracking-time/src/App.js
+++ tracking-time/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Components/Navbar';
+import Homepage from './Components/Homepage';
+import Footer from './Components/Footer';
+import Registration from './Components/Registration';
+import Login from './Components/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Homepage/>
+      <Footer/>
+      {/* <Registration/> */}
+      {/* <Login/> */}
     </div>
   );
 }
